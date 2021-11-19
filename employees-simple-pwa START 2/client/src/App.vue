2f9132c0118df6672cd078d1cd8dee0dec7f6b67
qhhivan <orsolic.i02@htlwienwest.at>
2021-11-19 14:30:34 +0100

    Start

---
+++ employees-simple-pwa START 2/client/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app" class="container d-flex flex-column justify-content-center align-items-center mt-5">
+    <ButtonGet @get="fetchData"></ButtonGet>
+    <CardView :employees="employees" @del="delEmployee"></CardView>
+  </div>
+</template>
+
+<script>
+import ButtonGet from '@/components/ButtonGet.vue';
+import CardView from '@/components/CardView.vue';
+
+export default {
+  name: 'app',
+  components: {
+    ButtonGet,
+    CardView,
+  },
+  data() {
+    return {
+      employees: [],
+    };
+  },
+  methods: {
+    fetchData() {
+      console.log('fetchData called');
+    },
+    delEmployee() {
+      console.log('delEmployee called');
+    },
+  },
+};
+</script>
+
+<style></style>
---
+++ employees-simple-pwa START 2/client/vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  pwa: {
+    manifestOptions: {
+      name: 'PWA Demo Employees',
+      short_name: 'PWA Demo',
+      theme_color: '#963484',
+      background_color: '#28c2ff',
+    },
+  },
+};
